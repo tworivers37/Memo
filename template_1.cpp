6b89b70dfd75cc8bfd6223e761a6bfbefa78790d
krquddnr37 <krquddnr37@naver.com>
2019-12-11 00:14:36 +0900

    - template_1 : 추가

--- template_1.cpp
+++ template_1.cpp
@@ -30,24 +30,62 @@ bool isTest2(T1 a, TN... b){
 
 template<typename T>
 void print2(T arg){
+    std::cout<<"print2 std::cout call\n";
     std::cout<<arg<<"\n";
 }
 
 template<typename T, typename... Types>
 void print2(T first, Types... args){
-    print2(first);
+    std::cout<<"print2 called\n";
+    print2(first); // first = args + args  //  first = args + 1
     print2(args...);
 }
 
 template<typename... T>
-void printAll(T const&... args){
+void printDouble(T const&... args){
+    std::cout<<"printDouble\n";
     print2(args + args...);
 }
 
+template<typename... T>
+void printAddOne(T const&... args){
+    std::cout<<"printAddOne\n";
+    print2(args + 1 ...); // 1 (space) ... // 1... -> 소수점으로 인식
+}
 
 int main(){
     std::cout<<isTest(1, 3.4, "56")<<"\n";
     std::cout<<isTest2(123, 1.2, 321)<<"\n";
-    printAll(1,10,100); // print2(1 + 1, 10 + 10, 100 + 100)
+    /*
+        0
+        a : 0
+        a : 1
+        1
+    */
+    std::cout<<std::endl;
+    printDouble(1,10,100); // print2(1 + 1, 10 + 10, 100 + 100)
+    std::cout<<std::endl;
+    printAddOne(1,10,100);
+    /*
+        printDouble
+        print2 called
+        print2 std::cout call
+        2
+        print2 called
+        print2 std::cout call
+        20
+        print2 std::cout call
+        200
+
+        printAddOne
+        print2 called
+        print2 std::cout call
+        2
+        print2 called
+        print2 std::cout call
+        11
+        print2 std::cout call
+        101
+    */
     return 0;
 }
